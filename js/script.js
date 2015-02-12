65829cdb2c180ce74b84f1acd2d22c0ccf018f7e
gathj07 <gath0807@gmail.com>
2015-02-12 09:04:07 -0600

    finished prototype

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ $(document).on("scroll",function(){
 
 /* smooth scrolling */
 var $root = $('html, body');
-$('a').click(function() {
+$("nav a").click(function() {
     var href = $(this.hash);
     href =href.length ? href : $('[id=' + this.hash.slice(1) +']');
     $root.animate({
@@ -59,31 +59,102 @@ $(window).scroll(function(){
 
 
 /* carousel */
-jQuery(function($){
-    var $carousel = $('.carousel');
-    var $slide = 'li';
-    var $transition = 1200;
-    var $time = 4000;
+
+/*$(document).ready(function(){
+    
+    $('.carousel ul').css({width: ($( window ).width()*3)});
+    $('.carousel li').css({width: ($( window ).width())});
+    $('.carousel img').css({width: ($( window ).width())});
+    $('.carousel').css({width: ($( window ).width())});
+	// Set the interval to be 5 seconds
+	var t = setInterval(function(){
+		$(".carousel ul").animate({marginLeft:-1*($(window).width())},1000,function(){
+			$(this).find("li:last").after($(this).find("li:first"));
+			$(this).css({marginLeft:0});
+		})
+	},4000);
+});
+
+$( window ).resize(function() {
+    $('.carousel ul').css({width: ($( window ).width()*3)});
+    $('.carousel li').css({width: ($( window ).width())});
+    $('.carousel').css({width: ($( window ).width())});
+    $('.carousel img').css({width: ($( window ).width())});
+});*/
+
+
+
+jQuery(function(){
+    var carousel = $('.carousel');
+    var transition = 1000;
+    var time = 4000;
     
     function slides(){
-        return $carousel.find($slide);   
+        return carousel.find('li');   
     }
     
     slides().fadeOut();
     
     slides().first().addClass('active');
-    slides().first().fadeIn($transition);
+    slides().first().fadeIn(transition);
     
-    $interval = setInterval(
+    interval = setInterval(
         function(){
-            var $i = $carousel.find($slide + '.active').index();
+            var i = carousel.find('li' + '.active').index();
+            slides().eq(i).fadeOut(transition);            
+            slides().eq(i).removeClass('active');
+
             
-            slides().eq($i).removeClass('active');
-            slides().eq($i).fadeOut($transition);
+            if(slides().length == i+1) i = -1;
             
-            if(slides().length == $i+1) $i = -1;
+            slides().eq(i+1).fadeIn(transition);
+            slides().eq(i+1).addClass('active');
+        }, transition+time );
+    
+    $("#leftArrow").on('click', function(){
+            var i = carousel.find('li' + '.active').index();
+            slides().eq(i).fadeOut(transition);            
+            slides().eq(i).removeClass('active');
+        
+            if(i == 0) 
+                i = slides().length - 1;
+            else 
+                i--;
+            slides().eq(i).fadeIn(transition);
+            slides().eq(i).addClass('active');
+    });
+
+    $("#rightArrow").on('click', function(){
+            var i = carousel.find('li' + '.active').index();
+            slides().eq(i).fadeOut(transition);            
+            slides().eq(i).removeClass('active');
+        
+            if(i == slides().length-1) 
+                i = 0;
+            else 
+                i++;
+            slides().eq(i).fadeIn(transition);
+            slides().eq(i).addClass('active');
+    });
             
-            slides().eq($i+1).fadeIn($transition);
-            slides().eq($i+1).addClass('active');
-        }, $transition+$time );
+});
+
+
+jQuery(function() {
+  
+  // Build modal
+  $(".open-modal").on('click', function(e) {
+    e.preventDefault();
+    var image = $(this).attr('href');
+    $('html').addClass('no-scroll');
+    $('body').append('<div class="modal"><img src="' + image + '">');
+    return false;
+  });
+  
+  // Close modal
+    $('body').on('click', '.modal', function() {
+    $('html').removeClass('no-scroll');
+    $('.modal').remove();
+  });
+  
 });
